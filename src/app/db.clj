51612f6e26a21a286ee3abd0c5f2279017cef153
Ownclo - Arcis <mrownclo@gmail.com>
2014-10-26 20:16:24 +0300

    Fixed korma entity declaration syntax

--- src/app/db.clj
+++ src/app/db.clj
@@ -6,37 +6,40 @@
                         :user "betty"
                         :password "betty"}))
 
+(declare account bet selection event selection_type market_type event_type)
+
 ;; Account, Bet and Selection are dynamically-changing entities.
 (defentity account
   (entity-fields :amount)
-  (has-many :bet)  ; bet.account_id = account.id
+  (has-many bet))  ; bet.account_id = account.id
 
 (defentity bet
   (entity-fields :amount :rate :risk)
-  (belongs-to :account)  ; has account_id field
-  (belongs-to :selection)) ; or just 'selection_type' string?
+  (belongs-to account)  ; has account_id field
+  (belongs-to selection)) ; or just have 'selection_type' string?
 
 (defentity selection
-  (has-many :bet)
-  (belongs-to :event)
-  (belongs-to :selection_type))
+  (has-many bet)
+  (belongs-to event)
+  (belongs-to selection_type))
 
 (defentity event
-  (belongs-to :event_type)) ; or just 'event_type' string?
+  (belongs-to event_type)) ; or just 'event_type' string?
 
 ;; Market_Type, Selection_Type, Event_Type are representing
 ;; a static tree of selection possibilities. Could be replaced
 ;; with a nested JSON object so that text strings are used as
 ;; identifiers instead of ids.
-(defentity selection_type
-  (belongs-to :market_type))
-
-(defentity market_type
-  (has-many :selection_type)
-  (belongs-to :event_type))
-
-(defentity event_type
-  (has_many :market_type))
-
 ;; Another possibility is to support a mapping from selection
 ;; to the set of bets. This would be a more NoSQL-ish solution.
+(defentity selection_type
+  (belongs-to market_type))
+
+(defentity market_type
+  (has-many selection_type)
+  (belongs-to event_type))
+
+(defentity event_type
+  (has-many market_type))
+
+;; CORRESPONDING DATABASE SCHEMAS (get them automatically?)
