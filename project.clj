acab11e0dc7beb5766ea753fd7157a3a16346b37
Ownclo - Arcis <mrownclo@gmail.com>
2014-10-26 14:36:35 +0300

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject app "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :main app.core
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [postgresql "9.1-901.jdbc4"]
+                 [org.clojure/tools.nrepl "0.2.5"]])
---
+++ src/app/core.clj
@@ -0,0 +1,6 @@
+(ns app.core)
+
+(def greeting "Hello World")
+
+(defn -main []
+  (println greeting))
